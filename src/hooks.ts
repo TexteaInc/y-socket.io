b888973e4f83cdf3190085a6b61b165d57db8764
exuanbo <exuanbo@protonmail.com>
2022-08-30 22:29:10 +0100

    feat: hook support custom equality function

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,28 +1,33 @@
+import { useDebugValue } from 'react'
 import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector'
-import shallow from 'zustand/shallow'
 
-import type { SocketIOProvider, SocketIOProviderState } from './provider'
-import { INITIAL_STATE } from './provider'
+import { INITIAL_STATE, SocketIOProvider, SocketIOProviderState } from './provider'
 
 const getInitialState = () => INITIAL_STATE
 const noop = () => {}
 
 export function useSocketIOProviderState (provider: SocketIOProvider | undefined): SocketIOProviderState
+
 export function useSocketIOProviderState<StateSlice> (
   provider: SocketIOProvider | undefined,
-  selector: (state: SocketIOProviderState) => StateSlice
+  selector: (state: SocketIOProviderState) => StateSlice,
+  equalityFn?: (a: StateSlice, b: StateSlice) => boolean
 ): StateSlice
+
 export function useSocketIOProviderState<StateSlice> (
   provider: SocketIOProvider | undefined,
-  selector?: ((state: SocketIOProviderState) => StateSlice | SocketIOProviderState)
+  selector?: (state: SocketIOProviderState) => StateSlice | SocketIOProviderState,
+  equalityFn?: (a: StateSlice | SocketIOProviderState, b: StateSlice | SocketIOProviderState) => boolean
 ) {
   const { getState = getInitialState, subscribe = () => noop } = provider ?? {}
   const selectState = selector ?? getState
-  return useSyncExternalStoreWithSelector(
+  const selectedState = useSyncExternalStoreWithSelector(
     subscribe,
     getState,
     getInitialState,
     selectState,
-    shallow
+    equalityFn
   )
+  useDebugValue(selectedState)
+  return selectedState
 }
